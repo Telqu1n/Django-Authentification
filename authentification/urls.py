16bdcfabe1d2aab42648cc011c1df1d874074c07
Jack Carney <jackcarney09@outlook.com>
2025-02-04 13:56:25 +0000

    Creating a login page, and making the user log in straight awya

--- authentification/urls.py
+++ authentification/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.register_view, name='register'),
     # path('', views.RegisterCreateView.as_view(), name='register'),
     path('thanks/', views.ThankyouView.as_view(), name='thanks'),
+    path('login/', views.login_view, name='login'),
+    path('thankyou/', views.Thankyou2View.as_view(), name='thankyou'),
 ]
--- authentification/views.py
+++ authentification/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
 
@@ -9,18 +10,38 @@ def register_view(request):
   if request.method == "POST":
     form = UserCreationForm(request.POST)
     if form.is_valid():
-      form.save()
-      return redirect('thanks')
+      login(request, form.save())
+      return redirect('thankyou')
+    return render(request, 'authentification/register.html', {'form': form})
+
   else:
     form = UserCreationForm()
     return render(request, 'authentification/register.html', {'form': form})
-  
+   
+   
+    
+def login_view(request): 
+    if request.method == "POST": 
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid(): 
+            login(request, form.get_user())
+            return redirect("thankyou")
+        return render(request, 'authentification/register.html', {'form': form})
 
+    else: 
+        form = AuthenticationForm()
+    return render(request, "authentification/login-page.html", { "form": form })  
+  
+   
 # class RegisterCreateView(CreateView):
 #     form_class = UserCreationForm
 #     template_name = 'authentification/register.html'
-#     success_url = '/'
+#     success_url = 'thanks'
 
 
 class ThankyouView(TemplateView):
     template_name = 'authentification/thanks.html'
+
+class Thankyou2View(TemplateView):
+    template_name = 'authentification/thankyou.html'
+   
