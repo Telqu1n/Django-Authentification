e14b034ea20d62efc2a9265fd41aa9b88d1acc09
Jack Carney <jackcarney09@outlook.com>
2025-02-04 09:53:39 +0000

    Configuring the files and create a user register form and view

--- LogIn_LogOut_SignUp/settings.py
+++ LogIn_LogOut_SignUp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentification',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'LogIn_LogOut_SignUp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
