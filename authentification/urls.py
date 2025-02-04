0f8c2d401f7140e219e2da1a72a846b5b953b4b1
Jack Carney <jackcarney09@outlook.com>
2025-02-04 11:06:46 +0000

    saving the user after submission, and rendering a thankyou template

--- authentification/urls.py
+++ authentification/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.register_view, name='register'),
+    # path('', views.RegisterCreateView.as_view(), name='register'),
+    path('thanks/', views.ThankyouView.as_view(), name='thanks'),
 ]
--- authentification/views.py
+++ authentification/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
+
+#Create your views here.
 
 def register_view(request):
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('thanks')
+  else:
     form = UserCreationForm()
     return render(request, 'authentification/register.html', {'form': form})
+  
+
+# class RegisterCreateView(CreateView):
+#     form_class = UserCreationForm
+#     template_name = 'authentification/register.html'
+#     success_url = '/'
+
+
+class ThankyouView(TemplateView):
+    template_name = 'authentification/thanks.html'
